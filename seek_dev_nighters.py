d10d163b6026e3e845d2699944806a75f70bff10
Ivanov Sergey <djapy@yandex.ru>
2017-07-26 18:39:16 +0300

    Makes the code more readable

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -3,15 +3,19 @@ from pytz import timezone
 from datetime import datetime
 
 
-def get_the_number_of_pages():
-    url_based = 'https://devman.org/api/challenges/solution_attempts/?page=1'
+def get_content_users(page):
+    url_based = 'https://devman.org/api/challenges/solution_attempts/?page={}'.format(page)
     content = requests.get(url_based)
     content_users = content.json()
+    return content_users
+
+
+def get_the_number_of_pages(content_users):
     the_number_of_pages = int(content_users['number_of_pages'])
-    return the_number_of_pages, content_users
+    return the_number_of_pages
 
 
-def get_users_info_from_the_first_page(content_users):
+def get_users_info(content_users):
     for info_users in content_users['records']:
         yield {'username': info_users['username'],
                'timestamp': info_users['timestamp'],
@@ -19,18 +23,17 @@ def get_users_info_from_the_first_page(content_users):
                }
 
 
-def get_users_info(page_count):
-    start_page = 2
-    for page in range(start_page, page_count):
-        url = 'http://devman.org/api/challenges/solution_attempts/?page={}'.format(page)
-        content = requests.get(url)
-        content = content.json()
+def get_users_info_from_the_first_page(content_users):
+    return get_users_info(content_users)
 
-        for info_users in content['records']:
-            yield {'username': info_users['username'],
-                   'timestamp': info_users['timestamp'],
-                   'timezone': info_users['timezone'],
-                  }
+
+def get_other_users_info(page_count):
+    start_page = 2
+    number_for_range = page_count + 1
+    for page in range(start_page, number_for_range):
+        content = get_content_users(page)
+        content_from_other_pagess = get_users_info(content)
+    return content_from_other_pagess
 
 
 def get_midnight_user(info):
@@ -44,23 +47,28 @@ def get_midnight_user(info):
 
 def get_set_users(content_user):
     set_users = set()
-    for user_info in content_user:
+    for user_info in (content_user):
         if get_midnight_user(user_info):
             set_users.add(user_info['username'])
     return set_users
 
 
-def show_information():
+def show_information(content_from_the_first_page, content_from_other_pages):
     if get_set_users(content_from_the_first_page):
         all_users_info = get_set_users(content_from_other_pages).update(content_from_the_first_page)
     all_users_info = get_set_users(content_from_other_pages)
-    print('Users are night owls:')
-    for number, user in enumerate(all_users_info):
-        print(number, user)
+    return all_users_info
+
 
 
 if __name__ == '__main__':
-    page_count, content = get_the_number_of_pages()
-    content_from_the_first_page = get_users_info_from_the_first_page(content)
-    content_from_other_pages = get_users_info(page_count)
-    show_information()
+    page = 1
+    content_users = get_content_users(page)
+    page_count = get_the_number_of_pages(content_users)
+    content_from_the_first_page = get_users_info_from_the_first_page(content_users)
+    content_from_other_pages = get_other_users_info(page_count)
+    all_users_info = show_information(content_from_the_first_page,
+                                      content_from_other_pages)
+    print('Users are night owls:')
+    for number, user in enumerate(all_users_info):
+        print(number, user)
